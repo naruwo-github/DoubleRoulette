6417a3938087e2888fd307231952c6dc9673da7f
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2019-11-29 21:58:47 +0900

    カラーを返すクラスを作成

---
+++ DoubleRoulette/ColorStock.swift
@@ -0,0 +1,13 @@
+//
+//  ColorStock.swift
+//  DoubleRoulette
+//
+//  Created by Narumi Nogawa on 2019/11/29.
+//  Copyright © 2019 Narumi Nogawa. All rights reserved.
+//
+
+import Foundation
+
+class ColorStock {
+    //約20パターンくらい用意しておいて、数値によって色を返す関数を実装する
+}
