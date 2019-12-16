7ccbb0a4a14dc6c69d04619bd2e13f11c303ae00
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2019-12-16 22:39:23 +0900

    コメント削除

--- DoubleRoulette/ViewController.swift
+++ DoubleRoulette/ViewController.swift
@@ -109,8 +109,6 @@ class ViewController: UIViewController, GADBannerViewDelegate {
         bannerView.rootViewController = self
         bannerView.load(GADRequest())
         bannerView.delegate = self
-        
-        print(bannerView.frame)
     }
     
     func setOuterRoulette(outerName: [String], outerColor: [UIColor]) {
