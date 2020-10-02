0ab4de04a95da47b8861f5d9d79d4c4c30e65ca2
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-10-02 21:07:00 +0900

    リファクタリング

--- DoubleRoulette/Model/MyPieChartView.swift
+++ DoubleRoulette/Model/MyPieChartView.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2019 Narumi Nogawa. All rights reserved.
 //
 
-//import Foundation
 import UIKit
 
 struct Segment {
@@ -35,19 +34,16 @@ class MyPieChartView: UIView {
     override func draw(_ rect: CGRect) {
         
         let ctx = UIGraphicsGetCurrentContext()
-        //let radius = min(frame.size.width, frame.size.height)/2
         let viewCenter = CGPoint(x: bounds.size.width/2, y: bounds.size.height/2)
         let valueCount = segments.reduce(0) {$0 + $1.value}
         
         var startAngle = -CGFloat(Double.pi * 0.5)
         
-        //draw circle
         ctx?.setFillColor(UIColor.gray.cgColor)
         ctx?.move(to: CGPoint(x: viewCenter.x, y: viewCenter.y))
         ctx?.addArc(center: CGPoint(x:viewCenter.x, y: viewCenter.y), radius: radius, startAngle: 0, endAngle: 360, clockwise: false)
         ctx?.fillPath()
         
-        //draw piechart
         for segment in segments {
             ctx?.setFillColor(segment.color.cgColor)
             let endAngle = startAngle+CGFloat(Double.pi * 2) * (segment.value / valueCount)
--- DoubleRoulette/ViewController/DRUserHelper.swift
+++ DoubleRoulette/ViewController/DRUserHelper.swift
@@ -8,46 +8,46 @@
 
 import Foundation
 
+// ユーザの情報を保存するためのクラス
 class DRUserHelper {
-    // NOTE: ユーザの情報を保存するためのクラス
     
-    // NOTE: アニメーションのセッティングを管理
+    // アニメーションのセッティングを管理
     enum AnimationSettingKey: String {
         case playSound = "dr.playSound"
         case displayResult = "dr.displayResult"
     }
     
-    // NOTE: データ保存のクラス関数
+    // データ保存のクラス関数
     class func save(_ key : String, value : Any?) {
         let userDefault = UserDefaults.standard
         userDefault.setValue(value, forKey: key)
     }
     
-    // NOTE: 指定データの削除のクラス関数
+    // 指定データの削除のクラス関数
     class func remove(_ key : String) {
         let userDefault = UserDefaults.standard
         userDefault.removeObject(forKey: key)
     }
     
-    // NOTE: 指定データの読み込みのクラス関数
+    // 指定データの読み込みのクラス関数
     class func load<T>(_ key : String, returnClass : T.Type) -> T? {
         let userDefault = UserDefaults.standard
         return userDefault.value(forKey: key) as? T
     }
     
-    // NOTE: 指定データの真偽値を返すクラス関数
+    // 指定データの真偽値を返すクラス関数
     class func bool(_ key : String) -> Bool {
         let userDefault = UserDefaults.standard
         return userDefault.bool(forKey: key)
     }
     
-    // NOTE: ユーザデータを全削除するクラス関数
+    // ユーザデータを全削除するクラス関数
     class func clearAll() {
         let userDefault = UserDefaults.standard
         userDefault.dictionaryRepresentation().forEach({ userDefault.removeObject(forKey: $0.0) })
     }
     
-    // NOTE: アニメーション中のサウンド管理（デフォルトはtrue）
+    // アニメーション中のサウンド管理（デフォルトはtrue）
     static var isAuthorizedPlaySound: Bool {
         get {
             return DRUserHelper.load(DRUserHelper.AnimationSettingKey.playSound.rawValue, returnClass: Bool.self) ?? true
@@ -57,7 +57,7 @@ class DRUserHelper {
         }
     }
     
-    // NOTE: アニメーション後の結果画面表示管理（デフォルトはtrue）
+    // アニメーション後の結果画面表示管理（デフォルトはtrue）
     static var isAuthorizedResultView: Bool {
         get {
             return DRUserHelper.load(DRUserHelper.AnimationSettingKey.displayResult.rawValue, returnClass: Bool.self) ?? true
