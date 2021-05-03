c6df4579e5f37d7acc6cd533886da40845ff06b9
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2021-05-03 17:24:03 +0900

    表示まわりの移行完了
    セルの状態編集がまだできない

--- DoubleRoulette/ViewController/DRSettingViewController.swift
+++ DoubleRoulette/ViewController/DRSettingViewController.swift
@@ -9,7 +9,7 @@
 // MARK: - <OS固有フレームワーク>
 import UIKit
 // MARK: - <外部フレームワーク>
-import AMColorPicker
+//import AMColorPicker
 import CellAnimator
 import Firebase
 import FirebaseAnalytics
@@ -23,6 +23,10 @@ class DRSettingViewController: UIViewController {
     @IBOutlet private weak var bottomBannerAdView: GADBannerView!
     @IBOutlet private weak var tableView: UITableView!
     
+    private let colorStock = ColorModel()
+    private var rouletteData: Results<RouletteObject>!
+    private var newCellId: Int = 0
+    
     // MARK: - <ライフサイクル>
     
     override func viewDidLoad() {
@@ -31,6 +35,12 @@ class DRSettingViewController: UIViewController {
         self.navigationController?.navigationBar.tintColor = R.color.navigationItemColor()
         self.setupAdvertisement()
         self.setupTableView()
+        self.roadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        Analytics.logEvent("show_cell_setting_view", parameters: nil)
     }
     
     // MARK: - <public関数>
@@ -48,7 +58,42 @@ class DRSettingViewController: UIViewController {
     
     private func setupTableView() {
         self.tableView.register(UINib(resource: R.nib.drTableViewCell), forCellReuseIdentifier: "RouletteCell")
-        self.tableView.rowHeight = 60
+        self.tableView.rowHeight = UIDevice.current.userInterfaceIdiom == .pad ? 80 : 60
+    }
+    
+    private func roadData() {
+        self.rouletteData = DRRealmHelper.init().getRouletteData()
+        self.newCellId = DRUserHelper.load("id", returnClass: Int.self) ?? 0
+        self.tableView.reloadData()
+    }
+    
+    // MARK: - <イベント登録(IBAction)>
+    @IBAction private func clearAllButtonTapped(_ sender: Any) {
+        DRRealmHelper.init().deleteAll()
+        self.tableView.reloadData()
+        self.newCellId = 0
+        DRUserHelper.save("id", value: 0)
+        Analytics.logEvent("all_clear_button", parameters: nil)
+    }
+    
+    @IBAction private func playButtonTapped(_ sender: Any) {
+        guard let rouletteVC = R.storyboard.main.drRouletteViewController() else { return }
+        self.navigationController?.pushViewController(rouletteVC, animated: true)
+    }
+    
+    @IBAction private func addButtonTapped(_ sender: Any) {
+        let newCell = RouletteObject()
+        newCell.id = self.newCellId
+        
+        self.newCellId += 1
+        DRUserHelper.save("id", value: self.newCellId)
+        
+        let colorRGB = UIColor.convertToRGB(self.colorStock.proposeColor(index: self.rouletteData.count))
+        newCell.color = UIColor.rgbToHex(red: Int(colorRGB.red * 255), green: Int(colorRGB.green * 255), blue: Int(colorRGB.blue * 255))
+        DRRealmHelper.init().add(object: newCell)
+        self.tableView.reloadData()
+        self.tableView.scrollToRow(at: IndexPath(row: rouletteData.count - 1, section: 0), at: .bottom, animated: true)
+        Analytics.logEvent("add_cell_button", parameters: nil)
     }
     
 }
@@ -57,13 +102,27 @@ class DRSettingViewController: UIViewController {
 extension DRSettingViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // TODO: 要修正
-        return 5
+        return self.rouletteData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RouletteCell") as! DRTableViewCell
+        let object = self.rouletteData[indexPath.row]
+        let rgb = UIColor.hexToRGB(hex: object.color)
+        cell.setupCell(type: object.type, name: object.item, color: UIColor.rgbToUIColor(red: rgb[0], green: rgb[1], blue: rgb[2]))
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        CellAnimator.animateCell(cell: cell, withTransform: CellAnimator.TransformTilt, andDuration: 1)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            DRRealmHelper.init().delete(object: self.rouletteData[indexPath.row])
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            self.tableView.reloadData()
+        }
     }
     
 }
