9e8057309cee6da4b3fe873cc526dd91bf05c47b
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2021-05-03 15:35:43 +0900

    セルのxib作成

---
+++ DoubleRoulette/ViewController/DRTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  DRTableViewCell.swift
+//  DoubleRoulette
+//
+//  Created by Narumi Nogawa on 2021/05/03.
+//  Copyright © 2021 Narumi Nogawa. All rights reserved.
+//
+
+// MARK: - <OS固有フレームワーク>
+import UIKit
+
+// MARK: - <ルーレットセルのクラス>
+class DRTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
